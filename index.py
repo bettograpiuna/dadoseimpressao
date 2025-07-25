b02dc2779d8e4468324b7ae12c434e1746c1e999
Betto <bettograpiuna@gmail.com>
2025-07-25 01:52:13 -0300

    Primeiro commit

---
+++ index.py
@@ -0,0 +1,5 @@
+
+nome = str(input("Para cadastro, informe seu nome: "))
+idade = int(input("Agora, sua idade: "))
+time = str(input("Para finalizar, qual time de futebol torce: "))
+print(f"Olá, {nome}, parabéns pelos {idade} anos torcendo e acompanhando o {time}!")
